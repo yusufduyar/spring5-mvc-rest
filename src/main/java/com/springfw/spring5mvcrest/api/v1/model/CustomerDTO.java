ee41d7958454334c8bacd883e9da89d8d0b3ca75
yusufduyar <yusufduyar@outlook.com>
2018-01-09 15:49:29 +0300

    Added swagger support

--- src/main/java/com/springfw/spring5mvcrest/api/v1/model/CustomerDTO.java
+++ src/main/java/com/springfw/spring5mvcrest/api/v1/model/CustomerDTO.java
@@ -1,10 +1,12 @@
 package com.springfw.spring5mvcrest.api.v1.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class CustomerDTO {
+    @ApiModelProperty(value = "First name of the customer",required = true)
     private String firstname;
     private String lastname;
     @JsonProperty("customer_url")
--- src/main/java/com/springfw/spring5mvcrest/controllers/v1/CustomerController.java
+++ src/main/java/com/springfw/spring5mvcrest/controllers/v1/CustomerController.java
@@ -3,9 +3,12 @@ package com.springfw.spring5mvcrest.controllers.v1;
 import com.springfw.spring5mvcrest.api.v1.model.CustomerDTO;
 import com.springfw.spring5mvcrest.api.v1.model.CustomerListDTO;
 import com.springfw.spring5mvcrest.services.ICustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@Api(description = "This is the controller for customer")
 @RestController
 @RequestMapping(CustomerController.BASE_URL)
 public class CustomerController {
@@ -18,6 +21,7 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @ApiOperation(value = "This will return a list of customers",notes = "Notes about API")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public CustomerListDTO getAllCustomers(){
